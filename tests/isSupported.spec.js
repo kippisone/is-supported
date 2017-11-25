50ea4c937596a18b589659c254bb1a07aa44380e
Andifeind <andifeind@noname-media.com>
2017-11-25 15:31:13 +0100

    Add travis conf, add LICENSE

---
+++ tests/isSupported.spec.js
@@ -0,0 +1,13 @@
+const inspect = require('inspect.js')
+const sinon = require('sinon')
+inspect.useSinon(sinon)
+
+const inspect = require('inspect.js')
+
+describe('Module', () => {
+  describe('asyncSupported', () => {
+    it('should return a boolean if async is supported or not', () => {
+      
+    })
+  })
+})
